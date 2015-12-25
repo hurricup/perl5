a9bc4df3773b0adbb0551bf470036bd1d085ba0e
David Mitchell <davem@iabyn.com>
2015-12-25 11:07:28 +0000

    rename POPFOO() to CX_POPFOO()
    Rename all the context-popping macros such as POPBLOCK and POPSUB, by
    giving them a CX_ prefix. (Do TOPBLOCK too).
    
    This is principally to  deliberately break any existing non-core use of
    these non-API macros, as their behaviour has changed in this branch.
    In particular, POPBLOCK(cx) no longer decrements the cxt stack pointer
    nor sets cx; instead, cx is now expected to already point to the stack
    frame which POPBLOCK should process.
    
    At the same time, giving them a CX_ prefix makes it clearer that these
    are all part of a family of macros that manipulate the context stack.
    
    The PUSHFOO() macros will be renamed in a later commit.

--- perl.c
+++ perl.c
@@ -5080,7 +5080,7 @@ S_my_exit_jump(pTHX)
     POPSTACK_TO(PL_mainstack);
     if (cxstack_ix >= 0) {
         dounwind(-1);
-        POPBLOCK(cxstack);
+        CX_POPBLOCK(cxstack);
     }
     LEAVE_SCOPE(0);
 
